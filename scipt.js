351b0802f8aa5c9c2374fa35f6f67d1922cd9a2f
Sani1308 <cunanansonnyboy7@gmail.com>
2024-11-30 20:53:16 +0800

    Initial commit

---
+++ scipt.js
@@ -0,0 +1,8 @@
+
+const button = document.getElementById('Submit-btn');
+button.addEventListener('click', () =>{
+    alert('Send Successful.');
+});
+function openHtmlFile() {
+    window.location.href = 'rent now/index.html'; 
+}
